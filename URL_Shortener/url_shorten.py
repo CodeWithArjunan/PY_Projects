630f06eaf05017c90fa74b4261bf43de5f55b443
CodeWithArjunan <arjunan.codes>
2025-07-25 21:57:40 -0700

    main_code

--- URL_Shortener/url_shorten.py
+++ URL_Shortener/url_shorten.py
@@ -1,10 +1,10 @@
-from flash import Flash,request,redirect,jsonify,render_template
-import mysql.Connector
+from flask import Flask,request,redirect,jsonify,render_template
+import mysql.connector
 import hashlib
 import base64
 
-app=Flash(__name__)
-
+app=Flask(__name__)
+#database configure
 DB_CONFIG = {'host':'localhost',
              'user':'root',
              'password':'root',
@@ -14,31 +14,71 @@ DB_CONFIG = {'host':'localhost',
 def db_connection():
     return mysql.connector.connect(**DB_CONFIG)
 
-
+#Genarate Short URL
 def generate_short_url(long_url):
     hash_obj= hashlib.sha256(long_url.encode())
     short_path = base64.urlsafe_b64encode(hash_obj.digest())
+    print(short_path)
     return short_path
 
-
+#Serve the HTML form
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("/index.html")
 
-@app.route('/shorten',method=['POST'])
+#Handle url shortening
+@app.route('/shorten', methods=['POST'])
 def shorten_url():
     long_url = request.form.get("long_url")
     if not long_url:
-        return "INVLID",400
+        return "INVLID: 400"
 
     conn = get_db_connector()
     curseor = conn.curser(dictionary=True)
 
+    #Check if URL exist
     curser.execute("SELECT short_url FROM url-mapping WHERE long_url=%s",(long_url,))
     existing_entry = cursor.fetchoe()
 
     if existing_entry:
-        return f"Shortedurl:<a href='{request.host_url}{existing_entry['short_url']}'>{request.host_url}{existing_entry['short_url']}</a>"
-        #return f"Shortedurl:<a href='{request.host_url}{existing_entry['short_url']}'>https://de/{existing_entry['short_url']}</a>"
+        #return f"Shortedurl:<a href='{request.host_url}{existing_entry['short_url']}'>{request.host_url}{existing_entry['short_url']}</a>"
+        return f"Shortedurl:<a href='{request.host_url}{existing_entry['short_url']}'>https://de/{existing_entry['short_url']}</a>"
 
+    short_url = generate_short_url()
+    cursor.execute("INSERT INTO url_maping(long_url,short_url) VALUE (%s,%s)",(long_url,short_url))
+    conn.commit()
+    conn,close()
+
+    return f"Shorted url: <a href='{request.host_url}{short_url}'>{request.hpst_url}{short_url}</a>"
+
+
+@app.route('/short_url', methods=['GET'])
+def redirect_url(shorturl):
+    conn.get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT long_url FROM url_mapping WHERE short_url=%s",(short_url,))
+
+    entry = cursor.fetchone()
+
+    if entry:
+        cursor.execute("UPDATE url_mapping SET clicks=clicks+1 WHERE short_url=%s",(short_url,))
+
+        conn.commit()
+        conn.close()
+
+        return redirect(entry("long_url"))
+    conn.close()
+    return "Error: URL not fount:404"
+
+#Run the fash application
+if __name__ == '__main__':
+    app.run(debug=True)
     
+
+
+
+
+
+
+
